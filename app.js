3c1b277695a842c7c0ebd0d4e763e6f90c586c98
mel <melissa.sargent@hotmail.co.uk>
2020-06-07 13:45:52 +0100

    fully working calc

--- app.js
+++ app.js
@@ -3,30 +3,46 @@ const numbers = document.querySelectorAll('.numbers button')
 const operators = document.querySelectorAll('.operators button');
 const answer = document.getElementById('equals')
 let array = []
+let operatorArray = []
 
-
-// numbers.addEventListener('click', () => {
-//   console.log("digit");
-// });
-
-// numbers.addEventListener('click', event => {
-//   console.log("pressed")
-// })
 numbers.forEach(button => {
-  button.addEventListener('click', event => {
+  button.addEventListener('click', () => {
     inpt.textContent += button.textContent
     array.push(button.textContent)
     console.log(array);
   })
 })
 operators.forEach(button => {
-  button.addEventListener('click', event => {
+  button.addEventListener('click', () => {
     array.push(button.textContent)
+    operatorArray.push(button.textContent)
     inpt.textContent += button.textContent
     console.log(array);
   })
 })
-answer.addEventListener('click', ()=>{
 
-    inpt.textContent += answer.textContent
-    });
+// };
+answer.addEventListener('click', ()=>{
+    inpt.textContent += answer.textContent;
+    console.log(array);
+    pos = array.findIndex(x => x ==="+" || x ==="-" || x === "x" || x === "÷");
+    firstNum = array.slice(0, pos).join("");
+    secondNum = array.splice(pos+1).join("");
+      if (operatorArray[0] === "+") {
+    inpt.textContent += (parseFloat(firstNum) + parseFloat(secondNum))
+    console.log(operatorArray);
+    // console.log(parseFloat(firstNum));
+    } else if 
+      (operatorArray[0] === "-") {
+        inpt.textContent += (parseFloat(firstNum) - parseFloat(secondNum))
+    } else if 
+      (operatorArray[0] === "x") {
+        inpt.textContent += (parseFloat(firstNum) * parseFloat(secondNum))
+    } else if 
+      (operatorArray[0] === "÷") {
+        inpt.textContent += (parseFloat(firstNum) / parseFloat(secondNum))
+    } else {
+      console.log(error);
+    }
+  
+})
